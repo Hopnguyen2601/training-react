db5ba8cd4b5ed366be56ec03c37c070708fe3b4a
Hop Nguyen <hopnguyen.dev@gmail.com>
2022-09-21 19:30:51 +0700

    [#7] Implement search on Home page

---
+++ arlyn-farm/src/components/SearchForm/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+SearchForm.propTypes = {
+
+};
+
+function SearchForm(props) {
+	return (
+		<div>
+			<h2>Search here</h2>
+		</div>
+	);
+}
+
+export default SearchForm;
